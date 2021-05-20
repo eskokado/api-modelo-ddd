67d74c711a6ff9e53c734781e3b239803e7063d1
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-20 15:29:29 -0300

    Create AutoMapper Profile classes

---
+++ Api.Infra.CrossCutting/Mappings/ModelToEntityProfile.cs
@@ -0,0 +1,15 @@
+using Api.Domain.Entities;
+using Api.Domain.Models;
+using AutoMapper;
+
+namespace Api.Infra.CrossCutting.Mappings
+{
+    public class ModelToEntityProfile: Profile
+    {
+        public ModelToEntityProfile()
+        {
+            CreateMap<UserEntity, UserModel>()
+                .ReverseMap();
+        }
+    }
+}
